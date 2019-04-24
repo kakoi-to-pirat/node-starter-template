aebb07e93576bde511c4e906985e3f7c5d638c90
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2019-04-24 18:10:29 +0300

    eslintsrc to json and edit console.info

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || process.env.LOCAL_INPUT_PORT;
 createApp()
   .then(app => {
     app.listen(PORT, () => {
-      console.info(`Pirate stories running at ${PORT} port`); // eslint-disable-line no-console
+      console.info(`Your app running at ${PORT} port`); // eslint-disable-line no-console
     });
   })
   .catch(err => {
